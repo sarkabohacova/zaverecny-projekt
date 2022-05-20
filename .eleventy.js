01f272bcd528c707700004241fca2329a6666a20
Šárka Boháčová <bohacova.sar@gmail.com>
2022-05-21 00:21:47 +0200

    anotheer commit

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,8 @@ module.exports = function(eleventyConfig) {
   // Zkopírovat css/ to _site/css/
   eleventyConfig.addPassthroughCopy("css");
 
+  eleventyConfig.addPassthroughCopy("js");
+
   return {
     // možné formáty šablon
     templateFormats: ["njk", "html", "md"],
